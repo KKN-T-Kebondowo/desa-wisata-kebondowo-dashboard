daf78e4f06f9ccc7438c67f2a0bf68a5d3e5414e
Fahrel <fahrel@students.undip.ac.id>
2023-07-16 16:40:51 +0700

    fix: login validation

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,11 +1,9 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../providers/authProvider';
-import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { Stack, IconButton, InputAdornment, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import Iconify from '../../../components/iconify';
-
-// ----------------------------------------------------------------------
 
 const LoginForm = () => {
   const { login, api } = useContext(AuthContext);
@@ -14,6 +12,7 @@ const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleClick = async () => {
     try {
@@ -23,16 +22,28 @@ const LoginForm = () => {
         password,
       });
 
-      const { access_token, refresh_token } = response.data;
+      if (response.status === 200) {
+        const { access_token, refresh_token } = response.data;
 
-      // Store the token in the context
-      login({ access_token, refresh_token });
+        // Store the token in the context
+        login({ access_token, refresh_token });
 
-      // Redirect to the home page
-      navigate('/');
+        // Redirect to the home page
+        navigate('/');
+      } else {
+        throw new Error('Invalid username or password');
+      }
     } catch (error) {
       // Handle login error
+      setError(error.message);
       console.error('Login failed:', error);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleClick();
     }
   };
 
@@ -44,6 +55,9 @@ const LoginForm = () => {
           label="Username address"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          error={error !== null}
+          helperText={error}
+          onKeyPress={handleKeyPress}
         />
 
         <TextField
@@ -52,6 +66,8 @@ const LoginForm = () => {
           type={showPassword ? 'text' : 'password'}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          error={error !== null}
+          helperText={error}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -61,17 +77,11 @@ const LoginForm = () => {
               </InputAdornment>
             ),
           }}
+          onKeyPress={handleKeyPress}
         />
       </Stack>
-
-      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <Checkbox name="remember" label="Remember me" />
-        <Link variant="subtitle2" underline="hover">
-          Forgot password?
-        </Link>
-      </Stack>
-
-      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}></Stack>
+      <LoadingButton fullWidth size="large" type="button" variant="contained" onClick={handleClick}>
         Login
       </LoadingButton>
     </>
